69796d589d0c54d759c1995cd105caa685332b54
Ankush Patal <pataleankush12@gmail.com>
2024-09-03 10:59:16 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const robot = require('robotjs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('Mobile device connected');
+
+  socket.on('moveMouse', (data) => {
+      robot.moveMouse(data.x, data.y);
+  });
+
+
+  // socket.on('click', () => {
+  //   robot.mouseClick();
+  // });
+
+  socket.on('leftClick', () => {
+    robot.mouseClick('left');
+  });
+  
+  socket.on('rightClick', () => {
+    robot.mouseClick('right');
+  });
+
+  socket.on('type', (text) => {
+    robot.typeString(text);
+  });
+
+  socket.on('scroll', (data) => {
+    robot.scrollMouse(data.scrollX, data.scrollY);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Mobile device disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ servert2.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const robot = require('robotjs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('Device connected');
+
+  // Join a room
+  socket.on('joinRoom', (roomId) => {
+    socket.join(roomId);
+    console.log(`Device joined room ${roomId}`);
+  });
+
+  socket.on('moveMouse', (data) => {
+    const { roomId, x, y } = data;
+    console.log('data', data)
+    io.to(roomId).emit('moveMouse', { x, y });
+  });
+
+  socket.on('leftClick', (roomId) => {
+    io.to(roomId).emit('leftClick');
+  });
+
+  socket.on('rightClick', (roomId) => {
+    io.to(roomId).emit('rightClick');
+  });
+
+  socket.on('type', (data) => {
+    const { roomId, text } = data;
+    io.to(roomId).emit('type', text);
+  });
+
+  socket.on('scroll', (data) => {
+    const { roomId, scrollX, scrollY } = data;
+    io.to(roomId).emit('scroll', { scrollX, scrollY });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Device disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
